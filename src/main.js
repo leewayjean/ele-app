f4e71b5ff825487f42e28ab233d9763a2316bb43
lee <1083488756@qq.com>
2019-05-13 22:35:21 +0800

    使用axios拦截器，增加请求/响应动画

--- src/main.js
+++ src/main.js
@@ -1,13 +1,40 @@
 import Vue from 'vue'
+//引入mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
 
+import { Indicator } from 'mint-ui';
+
+Vue.use(MintUI)
 
 
 Vue.config.productionTip = false;
 Vue.prototype.axios = axios
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    Indicator.open('加载中...');
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    Indicator.close();
+    return response;
+  }, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
 
 const vm = new Vue({
   router,
